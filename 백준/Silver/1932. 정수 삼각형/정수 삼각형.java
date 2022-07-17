6f6af101c9d18054e7a2d70ac61fcecb9f26ec98
lgj2022 <98953739+lgj2022@users.noreply.github.com>
2022-07-17 11:26:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 304 ms, Memory: 26308 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int[][] map;
+	static int ans = 0;
+	static int dr[] = { 1, 1 };
+	static int dc[] = { 0, 1 };
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(bf.readLine());
+
+		map = new int[n][n];
+
+		for (int i = 0; i < map.length; i++) {
+			Arrays.fill(map[i], -1);
+		}
+
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j <= i; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+
+				if (i > 0) {
+					if (j>0 && map[i - 1][j - 1] > map[i - 1][j]) {
+						map[i][j] = map[i - 1][j - 1] + map[i][j];
+					} else {
+						map[i][j] = map[i - 1][j] + map[i][j];
+					}
+				}
+
+				ans = Math.max(ans, map[i][j]);
+			}
+		}
+		
+		System.out.println(ans);
+	}
+
+}
