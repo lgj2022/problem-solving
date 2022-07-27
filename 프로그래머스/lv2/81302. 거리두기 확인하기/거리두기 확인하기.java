3a22766edca5822ef51f77a2452ab2fcb57657bf
lgj2022 <98953739+lgj2022@users.noreply.github.com>
2022-07-27 10:16:54 +0900

    [level 2] Title: 거리두기 확인하기, Time: 9.04 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -30,8 +30,7 @@ class Solution {
                 visit = new boolean [5][5];
                 visit[al.get(k)[0]][al.get(k)[1]]=true;
                 System.out.println("시작 로우"+ al.get(k)[0]+"시작 컬럼"+al.get(k)[1]);
-                //check(al.get(k)[0],al.get(k)[1],visit,0,0);
-                bfs(al.get(k),visit);
+                check(al.get(k)[0],al.get(k)[1],visit,0,0);
             }
             
             if(flag==1){
@@ -79,50 +78,6 @@ class Solution {
                 }
                 check(nrow,ncol,visit,dist+1,chk);
             }
-        }
-    }
-    
-    
-    void bfs(int [] current , boolean visit[][]){
-        Queue <int []> que = new LinkedList<>();
-
-        que.add(new int[] {current[0],current[1],0,0});
-        
-        while(!que.isEmpty()){
-            int tmp[] =que.poll();        
-            int row = tmp[0];
-            int col = tmp[1];
-            int dist = tmp[2];
-            int chk= tmp[3];
-            
-            if(map[row][col]=='P' && dist==1 ){
-            System.out.println(row+" "+col);
-            flag=1;
-            break;
-        }
-        if(map[row][col]=='P' && dist==2 && chk==1){
-            //System.out.println(row+" "+col);
-            flag=1;
-            break;
-        }
-       
-            
-         for(int i =0 ; i<4; i++){
-            int nrow = row + dr[i];
-            int ncol = col + dc[i];
-            
-            if(nrow >= 0 && nrow<5 && ncol>=0 && ncol<5 && map[nrow][ncol]!='X' && !visit[nrow][ncol]){
-                if(nrow==0){
-                System.out.println("nrow "+nrow+"ncol "+ncol);
-                }
-                visit[nrow][ncol]=true;
-                if(map[nrow][ncol]=='O'){
-                    chk=1;
-                }
-                que.add(new int [] {nrow,ncol,dist+1,chk});
-            }
-         }
-            
         }
     }
 }
