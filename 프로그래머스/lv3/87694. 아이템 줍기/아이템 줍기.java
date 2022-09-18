314004f5c85a68f499484542a10fd75c780fe96b
lgj2022 <98953739+lgj2022@users.noreply.github.com>
2022-09-18 10:59:49 +0900

    [level 3] Title: 아이템 줍기, Time: 0.25 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,99 +1,92 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+
+
+import java.util.*;
 
 public class Solution {
+    static int[] dc = {0, 1, 0, -1};
+    static int[] dr = {-1, 0, 1, 0};
+    static int [][] map;
+    static boolean[][] visited;
+    static int distance = Integer.MAX_VALUE;
 
-    // test
-    public static void main(String[] args) {
-        System.out.println(solution(new int[][] { { 1, 1, 3, 7 }, { 2, 2, 7, 4 }, { 4, 3, 6, 6 } }, 1, 2, 6, 6)); // 13
-    }
-
-    static class Rectangle {
-        int x1;
-        int y1;
-        int x2;
-        int y2;
-
-        public Rectangle() {
-            // TODO Auto-generated constructor stub
-        }
-
-        public Rectangle(int x1, int y1, int x2, int y2) {
-            super();
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-
-        public boolean isIn(int xt, int yt) {
-            if (x1 < xt && xt < x2 && y1 < yt && yt < y2) {
-                return true;
-            }
-            return false;
-        }
-
-    }
-
-    static int[][] map;
-    static int[] dr = { -1, 1, 0, 0 };
-    static int[] dc = { 0, 0, -1, 1 };
-    static List<Rectangle> rects;
-
+    
     public static int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        int maxX = Integer.MIN_VALUE, maxY = Integer.MIN_VALUE;
 
-        map = new int[102][102];
-        rects = new ArrayList<>();
+        for (int i = 0; i < rectangle.length; i++) {
+            for (int j = 0; j < rectangle[0].length; j++) {
+                rectangle[i][j] = rectangle[i][j] * 2;
+            }
+        }
+        characterX *= 2;
+        characterY *= 2;
+        itemX *= 2;
+        itemY *= 2;
 
-        for (int[] rec : rectangle) {
-            int xmin = rec[0] * 2;
-            int ymin = rec[1] * 2;
-            int xmax = rec[2] * 2;
-            int ymax = rec[3] * 2;
-            rects.add(new Rectangle(xmin, ymin, xmax, ymax));
-            for (int x = xmin; x <= xmax; x++) {
-                for (int y = ymin; y <= ymax; y++) {
-                    map[x][y] -= 1;
+        for (int[] point : rectangle) {
+            int lx = point[0];
+            int ly = point[1];
+            int rx = point[2];
+            int ry = point[3];
+            maxX = Math.max(maxX, rx);
+            maxY = Math.max(maxY, ry);
+        }
+        visited = new boolean[maxY + 1][maxX + 1];
+        map = new int[maxY + 1][maxX + 1];
+        
+        for (int [] point : rectangle){
+            fillrectangle(point);
+        }
+        for (int[] point : rectangle){
+            removeinside(point);
+        }
+        //bfs
+        getDistance(characterX,characterY,itemX,itemY);
+
+        
+        return distance/2;
+    }
+
+    private static void removeinside(int[] point) {
+        for (int i =point[1]+1; i<point[3]; i++){
+            for (int j= point[0]+1; j<point[2]; j++){
+                map[i][j]=0;
+            }
+        }
+    }
+
+    private static void getDistance(int characterX, int characterY, int itemX, int itemY) {
+        Queue<int []> queue = new LinkedList<>();
+        queue.add(new int []{characterY,characterX,0});
+        visited[characterY][characterX]=true;
+        while (!queue.isEmpty()){
+            int current[] = queue.poll();
+            int currentrow = current[0];
+            int currentcol = current[1];
+            int count = current[2];
+            //System.out.println(currentcol+" "+currentrow+" "+count);
+            if(currentcol==itemX && currentrow==itemY){
+                distance = Math.min(count,distance);
+                break;
+            }
+            for (int i =0; i<4; i++){
+                int ncol = currentcol+dc[i];
+                int nrow = currentrow+dr[i];
+                if(nrow>=0 && nrow<map.length && ncol>=0 && ncol<map[0].length && map[nrow][ncol]==1&&!visited[nrow][ncol]){
+                    visited[nrow][ncol]=true;
+                    queue.add(new int[]{nrow,ncol,count+1});
                 }
             }
         }
+    }
+    //{1,1,7,4}
+    private static void fillrectangle(int [] point) {
+        for (int i =point[1]; i<=point[3]; i++){
+            for (int j= point[0]; j<=point[2]; j++){
 
-        int cx = characterX * 2;
-        int cy = characterY * 2;
-        int ix = itemX * 2;
-        int iy = itemY * 2;
-
-        Queue<int[]> q = new LinkedList<>();
-        int nx = 0;
-        int ny = 0;
-        q.add(new int[] { cx, cy, 1 });
-        while (!q.isEmpty()) {
-            int[] p = q.poll();
-            cx = p[0];
-            cy = p[1];
-            if (cx == ix && cy == iy) {
-                return (p[2] - 1) / 2;
-            }
-            map[cx][cy] = p[2];
-            for (int i = 0; i < 4; i++) {
-                nx = cx + dr[i];
-                ny = cy + dc[i];
-                if (map[nx][ny] < 0 && !inRect(nx, ny)) {
-                    q.add(new int[] { nx, ny, p[2] + 1 });
-                }
+                map[i][j]=1;
             }
         }
-        return -1;
     }
 
-    private static boolean inRect(int nx, int ny) {
-        for (Rectangle r : rects) {
-            if (r.isIn(nx, ny)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
